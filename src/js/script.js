e1f41dc23c37c0882ee32d8f8a0b62b6c2aaca80
iwabuchi <iwabuti404@gmail.com>
2019-04-15 00:57:54 +0900

    Tweakpane　test

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,21 @@
-$(function(){
+const PARAMS = {
+    speed: 1,
+  };
 
+
+$(function(){
+    const pane = new Tweakpane();
+    pane.addInput(PARAMS, 'speed', {
+        step: 0.05,
+        min: 0,
+        max: 100,
+    });
+    
     $(".start").on("click",function(event){
+        $(this).parents(".section").find(".img-box").css("transition","all "+PARAMS.speed+"s")
         $(this).parents(".section").addClass("active")
+        console.log(PARAMS.speed)
+        
         return false
     })
     $(".reset").on("click",function(event){
